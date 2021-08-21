d989974c976bafbf2c232155683645dbd035a797
JCerna10 <201600853@ujmd.edu.sv>
2021-08-20 18:23:07 -0600

    agregando el menu

---
+++ Login/src/Proyecto/Menu.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto;
+
+/**
+ *
+ * @author JCERNA10
+ */
+public class Menu {
+    
+}
